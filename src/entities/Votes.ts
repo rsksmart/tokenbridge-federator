e373cff59fd13294e7e18289347e55526ea93151
Guilherme Soares <guilherme.soares@iovlabs.org>
2023-12-29 19:18:17 +0100

    Fixed comments

--- src/entities/Votes.ts
+++ src/entities/Votes.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn} from 'typeorm';
 
 @Entity()
 export class Votes {
@@ -10,4 +10,8 @@ export class Votes {
     voted: boolean;
     @Column({nullable: true})
     transactionData: string;
+    @CreateDateColumn()
+    createdAt: Date;
+    @UpdateDateColumn()
+    updatedAt: Date;
 }
